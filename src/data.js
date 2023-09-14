760f8b851f2abe2c38218a0b37e226255b7c60fb
Milyaz Kamil <milyazkamil@gmail.com>
2023-09-14 09:09:53 +0300

    created an array containing people's information

---
+++ src/data.js
@@ -0,0 +1,48 @@
+import person1 from "./assets/images/person-1.png";
+import person2 from "./assets/images/person-2.jpeg";
+import person3 from "./assets/images/person-3.jpeg";
+import person4 from "./assets/images/person-4.jpeg";
+import person5 from "./assets/images/person-5.jpeg";
+
+export const people = [
+  {
+    id: 1,
+    name: 'Milyaz Kamil',
+    job: 'WEB DEVELOPER',
+    image: person1,
+    description:
+      "I'm Milyaz Kamil, a Front-end Developer in the web sector with two years of experience. Throughout my career, I have created various projects, continuously striving for improvement.",
+  },
+  {
+    id: 2,
+    name: 'Susan Smith',
+    job: 'MANAGER',
+    image: person2,
+    description:
+      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry.",
+  },
+  {
+    id: 3,
+    name: 'Anna Johnson',
+    job: 'WEB DESIGNER',
+    image: person3,
+    description:
+      'Helvetica artisan kinfolk thundercats lumber blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+  },
+  {
+    id: 4,
+    name: 'Peter Jones',
+    job: 'INTERN',
+    image: person4,
+    description:
+      'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+  },
+  {
+    id: 5,
+    name: 'Bill Anderson',
+    job: 'THE BOSS',
+    image: person5,
+    description:
+      'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic.',
+  },
+];
